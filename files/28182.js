128686b4e65b33b46db866b6a835f8a0e45c3d73
lanaestel <fenek13estel@gmail.com>
2025-08-15 18:05:53 +0200

    Migrated reactions

---
+++ files/28182.js
@@ -0,0 +1,362 @@
+ReactionsPlugin.setConfig({
+  includeCategories: ["people", "foods", "activity", "places", "objects"],
+  //  elemSelector: '.post-rating',
+  customEmojis: [
+    {
+      id: "_paw",
+      url: "https://forumupload.ru/uploads/001b/ec/ce/267/192374.png",
+      category: "Wild",
+    },
+    {
+      id: "_bear",
+      url: "https://forumupload.ru/uploads/001b/ec/ce/267/68424.png",
+      category: "Wild",
+    },
+    {
+      id: "_owl1",
+      url: "https://forumupload.ru/uploads/001b/ec/ce/267/51653.png",
+      category: "Wild",
+    },
+    {
+      id: "_owl2",
+      url: "https://forumupload.ru/uploads/001b/ec/ce/267/483512.png",
+      category: "Wild",
+    },
+    {
+      id: "_squi",
+      url: "https://forumupload.ru/uploads/001b/ec/ce/267/266216.png",
+      category: "Wild",
+    },
+    {
+      id: "_batty",
+      url: "https://forumupload.ru/uploads/001b/ec/ce/267/186891.png",
+      category: "Wild",
+    },
+    {
+      id: "_foxy",
+      url: "https://forumupload.ru/uploads/001b/ec/ce/267/448215.png",
+      category: "Wild",
+    },
+    {
+      id: "_hugs",
+      url: "https://forum4.ru/uploads/001b/ec/ce/267/84780.png",
+      category: "Wild",
+    },
+    {
+      id: "_frog",
+      url: "https://forumstatic.ru/files/001b/ec/ce/39427.png",
+      category: "Wild",
+    },
+    {
+      id: "_snek",
+      url: "https://upforme.ru/uploads/001b/ec/ce/267/777479.png",
+      category: "Wild",
+    },
+    {
+      id: "_butf",
+      url: "https://upforme.ru/uploads/001b/ec/ce/267/79648.png",
+      category: "Wild",
+    },
+    {
+      id: "_peng",
+      url: "https://upforme.ru/uploads/001b/ec/ce/267/50560.png",
+      category: "Wild",
+    },
+    {
+      id: "_cher",
+      url: "https://upforme.ru/uploads/001b/ec/ce/267/939414.png",
+      category: "Wild",
+    },
+    {
+      id: "_alpaca",
+      url: "https://upforme.ru/uploads/001b/ec/ce/267/486599.png",
+      category: "Wild",
+    },
+    {
+      id: "_golub",
+      url: "https://upforme.ru/uploads/001b/ec/ce/267/840516.png",
+      category: "Wild",
+    },
+    {
+      id: "_dogge",
+      url: "https://forum4.ru/uploads/001b/ec/ce/267/86344.png",
+      category: "Personal",
+    },
+    {
+      id: "_sadsad",
+      url: "https://forum4.ru/uploads/001b/ec/ce/267/314978.png",
+      category: "Personal",
+    },
+    {
+      id: "_nonono",
+      url: "https://forum4.ru/uploads/001b/ec/ce/267/863026.png",
+      category: "Personal",
+    },
+    {
+      id: "_hungup",
+      url: "https://forum4.ru/uploads/001b/ec/ce/267/603012.png",
+      category: "Personal",
+    },
+    {
+      id: "_fiiiiine",
+      url: "https://forumstatic.ru/files/001b/ec/ce/15038.png",
+      category: "Personal",
+    },
+    {
+      id: "_cat1",
+      url: "https://upforme.ru/uploads/0013/63/cd/2/773045.png",
+      category: "Personal",
+    },
+    {
+      id: "_doggo",
+      url: "https://upforme.ru/uploads/0013/63/cd/2/620369.png",
+      category: "Personal",
+    },
+    {
+      id: "_cat2",
+      url: "https://upforme.ru/uploads/0013/63/cd/2/503795.png",
+      category: "Personal",
+    },
+    {
+      id: "_cat3",
+      url: "https://upforme.ru/uploads/0013/63/cd/2/627617.png",
+      category: "Personal",
+    },
+    {
+      id: "_cutie1",
+      url: "https://forumstatic.ru/files/0013/b7/c4/19804.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie2",
+      url: "https://forumstatic.ru/files/0013/b7/c4/53278.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie3",
+      url: "https://forumstatic.ru/files/0013/b7/c4/27512.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie4",
+      url: "https://forumstatic.ru/files/0013/b7/c4/99426.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie5",
+      url: "https://forumstatic.ru/files/0013/b7/c4/28305.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie6",
+      url: "https://forumstatic.ru/files/0013/b7/c4/34400.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie7",
+      url: "https://forumstatic.ru/files/0013/b7/c4/29045.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie8",
+      url: "https://forumstatic.ru/files/0013/b7/c4/92895.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie9",
+      url: "https://forumstatic.ru/files/0013/b7/c4/48801.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie10",
+      url: "https://forumstatic.ru/files/0013/b7/c4/53227.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie11",
+      url: "https://forumstatic.ru/files/0013/b7/c4/19699.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie12",
+      url: "https://forumstatic.ru/files/0013/b7/c4/90899.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie13",
+      url: "https://forumstatic.ru/files/0013/b7/c4/69845.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie14",
+      url: "https://forumstatic.ru/files/0013/b7/c4/29101.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie15",
+      url: "https://forumstatic.ru/files/0013/b7/c4/64665.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie16",
+      url: "https://forumstatic.ru/files/0013/b7/c4/83017.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie17",
+      url: "https://forumstatic.ru/files/0013/b7/c4/46883.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie18",
+      url: "https://forumstatic.ru/files/0013/b7/c4/94108.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie19",
+      url: "https://forumstatic.ru/files/0013/b7/c4/22980.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_cutie20",
+      url: "https://forumstatic.ru/files/0013/b7/c4/28277.gif",
+      category: "Cutie",
+    },
+    {
+      id: "_emoji1",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/599491.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji2",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/35829.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji3",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/951474.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji4",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/774801.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji5",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/588600.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji6",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/540604.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji7",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/961014.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji8",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/799071.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji9",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/836814.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji10",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/443120.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji11",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/85207.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji12",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/358225.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji13",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/111609.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji14",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/744589.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji15",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/506876.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji16",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/104585.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji17",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/752491.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji18",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/529127.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji19",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/457564.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji20",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/802696.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji21",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/869766.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji22",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/211537.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji23",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/168598.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji24",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/241651.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji25",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/614982.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji26",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/624456.webp",
+      category: "Emoji",
+    },
+    {
+      id: "_emoji127",
+      url: "https://upforme.ru/uploads/001b/ff/2a/3/899791.webp",
+      category: "Emoji",
+    },
+  ],
+  includeForumIds: [1, 2, 3, 4, 13, 12, 11, 10],
+});
